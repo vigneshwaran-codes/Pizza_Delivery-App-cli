6a719db27981e1ffa4c4169299cc5b782c456717
vigneshwarank00 <smilevicky2000@gmail.com>
2022-11-29 17:47:08 +0530

    init repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Homescreen from './screens/Homescreen'
+import Cartscreen from './screens/Cartscreen'
+import Registerscreen from './screens/Registerscreen'
+import Loginscreen from './screens/Loginscreen'
+import Ordersscreen from './screens/Ordersscreen'
+import Adminscreen from './screens/Adminscreen'
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <BrowserRouter>
+        <Route path='/' exact component={Homescreen} />
+        <Route path='/cart' exact component={Cartscreen} />
+        <Route path='/register' exact component={Registerscreen} />
+        <Route path='/login' exact component={Loginscreen} />
+        <Route path='/orders' exact component={Ordersscreen} />
+        <Route path='/admin' component={Adminscreen} />
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
